0f6257d8d2f5ccb741924b9c03150db231fe84b1
Thays Figueiredo <112645696+ThayNyan@users.noreply.github.com>
2022-11-25 11:36:07 -0300

    JS Statements

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ function myFunction() {
     document.getElementById("demo5").innerHTML = "Mudando o parágrado!"
 
     document.getElementById("inner").innerHTML = 5 + 6;
+
+    document.getElementById("demo7").innerHTML = "Olá, amigo!";
+    document.getElementById("demo8").innerHTML = "Como estás?";
 }
